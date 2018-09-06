18a31c13327c9ef247c5320d19c9ee0782f4e56f
Valentas Milčius <valm@danskebank.lt>
2018-09-06 16:05:53 +0300

    Allow CORS

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const matchesController = require("./routes/matches");
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get("/status", (req, res, next) => {
     res.sendStatus(200);
 });
